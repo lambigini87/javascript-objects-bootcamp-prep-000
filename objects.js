04b5785707e5b1dcd26209c92e38cbb10380a563
Harrison tran <harrisontran@outlook.com>
2018-04-22 00:28:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   artistName: "songtitle"
 };
 
-function updatePlayList(playlist, artistName, songTitle){
+function updatePlaylist(playlist, artistName, songTitle){
   
   playlist[artistName] = songTitle
   
