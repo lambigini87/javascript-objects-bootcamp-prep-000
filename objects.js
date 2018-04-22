2f76f5e33345327c6fcbcad30a73059ffd3d1e35
Harrison tran <harrisontran@outlook.com>
2018-04-22 00:20:23 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var meals = {breakfast: "oatmeal"};
+meals = {
+  breakfast: 'avocado',
+  lunch: 'avocado',
+  dinner: 'avocado'
+}
+
+const firstMeal = 'breakfast';
+var meals = {firstMeal: 'oatmeal' }
