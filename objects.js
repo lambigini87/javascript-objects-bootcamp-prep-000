7d25b477a9174d7d50b6e170715fbcab821c8134
Harrison tran <harrisontran@outlook.com>
2018-04-22 00:29:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName, songTitle){
+function removeFromPlaylist(playlist, artistName){
   
   playlist[artistName] = songTitle
   
